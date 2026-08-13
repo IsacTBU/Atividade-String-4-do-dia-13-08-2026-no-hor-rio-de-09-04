4ff95d581ccadddb18b480a704997d00939afea3
IsacTBU <isac.umeda@escola.pr.gov.br>
2026-08-13 09:43:34 -0300

    string4.js

--- string4.js
+++ string4.js
@@ -1,8 +1,8 @@
-const nomeCompleto = "ana maria silva";
-const iniciais = nomeCompleto
-  .split(" ")
-  .map(nome => nome[0].toUpperCase())
-  .join("");
-
-console.log(iniciais); 
-
+const nome = "ana maria silva";
+const novoNome = nome.split(" ")
+console.log(novoNome)
+let letra1 = novoNome[0][0].toUpperCase()
+let letra2 = novoNome[1][0].toUpperCase()
+let letra3 = novoNome[2][0].toUpperCase()
+let final = letra1+letra2+letra3
+console.log(final)
