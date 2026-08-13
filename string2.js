dd5c883c2d0f6c3826a8488c1ef2f25a25b1efde
IsacTBU <isac.umeda@escola.pr.gov.br>
2026-08-13 09:19:17 -0300

    string2.js

--- string2.js
+++ string2.js
@@ -1,5 +1,5 @@
-const titulo = "Como criar um site";
-const slug = titulo.toLowerCase().split(" ").join("-");
+const post = "Como criar um site";
+const slug = post.toLowerCase().split(" ").join("-");
 
 console.log(slug); 
 
